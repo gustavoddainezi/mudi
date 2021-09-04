6c78461490e8d24b237195e0d83a758de92014ff
Gustavo Damaceno Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2021-09-04 18:51:53 -0300

    JPA inclusion and database connection

--- mudi/src/main/java/br/com/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -1,9 +1,13 @@
 package br.com.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,15 +16,14 @@ import br.com.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("O último desejo - The Witcher");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/41Yy6AbJXAL._SX331_BO1,204,203,200_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/%C3%BAltimo-desejo-Witcher-bruxo-Geralt/dp/854690281X/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=1630184049&sr=1-2");
-		pedido.setDescricao("Uma descrição qualquer para o pedido");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
+		List <Pedido> pedidos = query.getResultList();
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
