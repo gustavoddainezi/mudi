ef1a9100d36e9c39e12dc7f67dacee147b6113a7
Gustavo Damaceno Dainezi <gustavo.dainezi@fatec.sp.gov.br>
2021-09-05 03:00:52 -0300

    PedidoRepository and Dependency Injection createds

--- mudi/src/main/java/br/com/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -2,31 +2,28 @@ package br.com.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.mvc.mudi.model.Pedido;
+import br.com.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
-	
-	@PersistenceContext
-	private EntityManager entityManager;
-	
+
+	@Autowired
+	private PedidoRepository repository;
+
 	@GetMapping("/home")
 	public String home(Model model) {
-		
-		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-		List <Pedido> pedidos = query.getResultList();
+
+		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
+
 		model.addAttribute("pedidos", pedidos);
-		
+
 		return "home";
 	}
-	
+
 }
